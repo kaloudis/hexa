65ce2e0156485000e86d9c06488b7777fb4c0cf0
Muneer PP <abdulmuneer22@gmail.com>
2020-08-12 15:40:03 +0530

    added sentry

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-import "./shim";
-import { AppRegistry } from "react-native";
-import App from "./App";
-import { name as appName } from "./app.json";
+import './shim';
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+import * as Sentry from '@sentry/react-native';
+
+// TODO -- make changes as per env
+
+Sentry.init({
+  dsn:
+    'https://af9dddc9bd6943a3a2c6bffb46e6b79e@o433150.ingest.sentry.io/5387760',
+});
 
 AppRegistry.registerComponent(appName, () => App);
