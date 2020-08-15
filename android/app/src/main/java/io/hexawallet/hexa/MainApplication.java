574431c2c31dece5f7329eafcb244da5cc585f3f
Hemant Kumar Sain <hemantkumar.sain@sadhan.co.in>
2020-08-15 14:41:19 +0530

    Upgrade RN version to 0.63 via manual verion checking.

--- android/app/src/main/java/io/hexawallet/hexa/MainApplication.java
+++ android/app/src/main/java/io/hexawallet/hexa/MainApplication.java
@@ -1,56 +1,26 @@
 package io.hexawallet.hexa;
 
 import android.app.Application;
-
+import android.content.Context;
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.rpt.reactnativecheckpackageinstallation.CheckPackageInstallationPackage;
-import com.reactcommunity.rnlocalize.RNLocalizePackage;
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
-import com.gantix.JailMonkey.JailMonkeyPackage;
-import com.rt2zz.reactnativecontacts.ReactNativeContacts;
-import com.chirag.RNMail.*;
-import com.christopherdro.RNPrint.RNPrintPackage;
-import cl.json.RNSharePackage;
-import cl.json.ShareApplication;
-import org.reactnative.camera.RNCameraPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.tradle.react.UdpSocketsModule;
-import com.peel.react.TcpSocketsModule;
-import com.peel.react.rnos.RNOSModule;
-import com.reactnativecommunity.netinfo.NetInfoPackage;
-import com.horcrux.svg.SvgPackage;
-import com.kishanjvaghela.cardview.RNCardViewPackage;
-import com.brentvatne.react.ReactVideoPackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.bitgo.randombytes.RandomBytesPackage;
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
+
 import com.facebook.soloader.SoLoader;
-import io.hexawallet.hexa.generated.BasePackageList;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.swmansion.reanimated.ReanimatedPackage;
-import com.swmansion.rnscreens.RNScreensPackage;
 
-import org.unimodules.adapters.react.ReactAdapterPackage;
-import org.unimodules.adapters.react.ModuleRegistryAdapter;
-import org.unimodules.adapters.react.ReactModuleRegistryProvider;
-import org.unimodules.core.interfaces.Package;
-import org.unimodules.core.interfaces.SingletonModule;
-import expo.modules.constants.ConstantsPackage;
-import expo.modules.permissions.PermissionsPackage;
-import expo.modules.filesystem.FileSystemPackage;
-
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.List;
-import com.crashlytics.android.Crashlytics;
+
 import io.fabric.sdk.android.Fabric;
-
+import com.crashlytics.android.Crashlytics;
 import io.hexawallet.hexa.PdfPasswordPackage;
-
 import io.invertase.firebase.RNFirebasePackage;
-import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
+import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
 import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 
 public class MainApplication extends Application implements ShareApplication, ReactApplication {
@@ -70,17 +40,15 @@ public class MainApplication extends Application implements ShareApplication, Re
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(new MainReactPackage(),
-            new CheckPackageInstallationPackage(),
-            new RNLocalizePackage(),
-            new ReactNativeConfigPackage(),
-            new JailMonkeyPackage(), new RNFirebasePackage(), new RNFirebaseMessagingPackage(),new RNFirebaseAnalyticsPackage(),
-          new RNFirebaseNotificationsPackage(), new ReactNativeContacts(), new RNMail(),
-          new RNPrintPackage(), new RNSharePackage(), new RNCameraPackage(), new VectorIconsPackage(),
-          new UdpSocketsModule(), new TcpSocketsModule(), new RNOSModule(), new NetInfoPackage(), new SvgPackage(),
-          new RNCardViewPackage(), new ReactVideoPackage(), new RNDeviceInfo(),
-          new RandomBytesPackage(), new ReanimatedPackage(), new RNGestureHandlerPackage(), new RNScreensPackage(),
-          new PdfPasswordPackage(), new ModuleRegistryAdapter(mModuleRegistryProvider));
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      packages.add(new RNFirebaseMessagingPackage());
+      packages.add(new RNFirebaseCrashlyticsPackage());
+      packages.add(new RNFirebaseAnalyticsPackage());
+      packages.add(new RNFirebaseNotificationsPackage());
+      return packages;
     }
 
     @Override
@@ -99,5 +67,36 @@ public class MainApplication extends Application implements ShareApplication, Re
     super.onCreate();
     Fabric.with(this, new Crashlytics());
     SoLoader.init(this, /* native exopackage */ false);
+    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+  }
+
+  /**
+   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
+   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+   *
+   * @param context
+   * @param reactInstanceManager
+   */
+  private static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
+    if (BuildConfig.DEBUG) {
+      try {
+        /*
+         We use reflection here to pick up the class that initializes Flipper,
+        since Flipper library is not available in release mode
+        */
+        Class<?> aClass = Class.forName("io.hexawallet.hexa.ReactNativeFlipper");
+        aClass
+            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+            .invoke(null, context, reactInstanceManager);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      } catch (InvocationTargetException e) {
+        e.printStackTrace();
+      }
+    }
   }
 }
